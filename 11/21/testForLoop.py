fe352bd273a944608bef184f4fc48c4b4fb6f28e
lill74 <a0103932@naver.com>
2020-11-21 14:05:02 +0900

    Update testForLoop.py

--- 11/21/testForLoop.py
+++ 11/21/testForLoop.py
@@ -1,3 +1,18 @@
+# 문자들을 모아 놓았음으로 문자 '열' (List)
+
 string = "Lee Soul"
 for letter in string:
   print(letter)
+
+# Inside 'list' books each of A B C is called 'Element'
+books = ["A", "B", "C"]
+for book in books:
+  print(book)
+  
+for i in range(5):
+  print("Lee")
+  print(i)
+  
+# Range and skip : reversed for-loop
+for i in range(10, 5, -1):
+  print(i)
