47cb8f765f3ccd9cea5d33ac0e63caa9349c38d3
lill74 <a0103932@naver.com>
2020-11-21 13:53:10 +0900

    Create testConditionalStatement.py

---
+++ 11/21/testConditionalStatement.py
@@ -0,0 +1,14 @@
+# Warn! In python indent(들여쓰기) is so important. Be careful!
+
+num = int(input("Enter : "))
+
+if num > 0:
+  print("Postive!")
+  print(">0")
+  print("End.")
+elif num < 0:
+  print("Negative!")
+  print("<0")
+  print("End.")
+else:
+  print("Zero.")
