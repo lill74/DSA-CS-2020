bee27f4bf7fa6e400b96b10b4cd0ba0c68168311
lill74 <a0103932@naver.com>
2020-11-21 13:47:10 +0900

    Update testFour.py

--- 11/21/testFour.py
+++ 11/21/testFour.py
@@ -1,3 +1,14 @@
 name = "Lee"
 print(type(name))
-# Output: <class 'int>
+# Output: <class 'int'>
+
+name = 1.0
+print(type(name))
+# Output: <class 'float'>
+
+age = input("What is your age?")
+# input의 값은 str을 갖는다
+age = int(age) + 1
+# str에서 int로 형변환(Casting)을 해준다
+print("My name is " + str(age))
+# int에서 str로 형변환(Casting)을 해준다
